ce046868251812756630303bf35e6cc1611b736d
nshalman-github@elys.com <nshalman@elys.com>
2013-05-30 16:09:11 -0400

    move to base64-13.1.0, remote LD_LIBRARY_PATH evil

--- README.md
+++ README.md
@@ -6,5 +6,5 @@ Build scripts for building Spice-enabled QEMU for SmartOS
 Full documentation is kept on the wiki Page:
 http://wiki.smartos.org/display/DOC/Spice+on+SmartOS
 
-1. Create a multiarch-12.4.1 zone with a delegated dataset and log in to it as root.
+1. Create a base64-13.1.0 zone with a delegated dataset and log in to it as root.
 1. curl -k https://raw.github.com/nshalman/qemu-smartos-build/master/full-build | bash
--- config.sh
+++ config.sh
@@ -1,13 +1,13 @@
 export MAKEFLAGS="-j$(psrinfo | wc -l)"
 export LC_ALL=C
 export MY_PREFIX=/opt/local
-export MY_LIBS=${MY_PREFIX}/lib/amd64
+export MY_LIBS=${MY_PREFIX}/lib
 export MY_INCS=${MY_PREFIX}/include
 export LDFLAGS="-m64 -L${MY_LIBS}"
 export CFLAGS="-m64 -I${MY_INCS}"
 export CXXLAGS=${CFLAGS}
 export CPPFLAGS="-I${MY_INCS}"
-export SMARTDC=/smartdc2
+export SMARTDC=/opt/local
 
 export QEMU_VERSION=qemu-kvm-1.1.2
 export VERSION=1.0.1
